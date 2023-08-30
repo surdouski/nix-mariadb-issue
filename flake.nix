547b497240d2dd2ba671428ce2b0f32833d80d20
surdouski <michael.surdouski@gmail.com>
2023-08-29 20:36:20 -0400

    flake changed with branch?

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Application packaged using poetry2nix";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
